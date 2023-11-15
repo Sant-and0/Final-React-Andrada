a261c5dc03584ac6b1d209d1ae3a33ed2be3c1df
Sant-and0 <133072543+Sant-and0@users.noreply.github.com>
2023-11-15 11:00:21 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import './App.css'
-import { ItemListContainer } from "./components/ItemListContainer"
-import { ItemDetailsContainer } from "./components/ItemDetailsContainer"
-import { NavBar } from './components/NavBar'
+import './App.css';
+import { ItemListContainer } from './components/ItemListContainer';
+import { ItemDetailsContainer } from './components/ItemDetailsContainer';
+import { NavBar } from './components/NavBar';
 
 function App() {
   return (
@@ -19,6 +19,6 @@ function App() {
   </BrowserRouter>
     </>
     )
-}
+};
 
 export default App;
