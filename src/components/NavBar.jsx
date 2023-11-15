6947ac0a73a2605387650c3247d2c521b06d21b4
Sant-and0 <133072543+Sant-and0@users.noreply.github.com>
2023-11-15 11:07:28 -0300

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,28 +1,31 @@
-import Container from 'react-bootstrap/Container'
-import { NavLink } from 'react-router-dom'
+import Container from 'react-bootstrap/Container';
+import { NavLink } from 'react-router-dom';
 
-import { products } from '../data/products'
-import Nav from 'react-bootstrap/Nav'
-import Navbar from 'react-bootstrap/Navbar'
+import { products } from '../data/products';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-import { CartWidget } from "./CartWidget"
+import { CartWidget } from './CartWidget';
+
+  const categories = products.map ((item) => item.category);
+  const uniqueCategories = new Set(categories);
 
 export const NavBar = () => {
-
-  const categories = products.map ((item) => item.category)
-  const uniqueCategories = new Set(categories)
-
     return (<>
         <Navbar bg="dark" data-bs-theme="dark">
         <Container>
           <NavLink to="/">
-            <NavBar>SofTech</NavBar>
+            <NavBar.Brand>SofTech</NavBar.Brand>
           </NavLink>
           <Nav className="me-auto">
-          {[...uniqueCategories].map(item => <Nav.Link as={NavLink} key={item} to={'/category/${item}'}>{item}</Nav.Link>)}
+          {[...uniqueCategories].map(item => 
+            <Nav.Link as={NavLink} key={item} to={'/category/${item}'}>
+              {item}
+            </Nav.Link>
+            )}
           </Nav>
           <CartWidget />
         </Container>
       </Navbar>
             </>)
-}
+};
