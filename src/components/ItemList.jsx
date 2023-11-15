5cf982b6013f2b8bd34a699a540a4ec885fd8b35
Sant-and0 <133072543+Sant-and0@users.noreply.github.com>
2023-11-15 10:46:17 -0300

    Update ItemList.jsx

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,6 +1,6 @@
-import Container from "react-bootstrap/Container"; 
+import Container from "react-bootstrap/Container"
 
-import { Item } from "./Item";
+import { Item } from "./Item"
 
 export const ItemList = ({items} ) => {
 
